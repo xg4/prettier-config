41582f2c6146129c567e697fff7936584022cbdc
xg4 <xingor4@gmail.com>
2023-07-18 06:26:21 +0000

    feat: add prettier-plugin-tailwindcss

---
+++ index.js
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Options} */
+export default {
+  printWidth: 120,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  plugins: ['prettier-plugin-tailwindcss'],
+}
