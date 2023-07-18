79181c06538bd75ff76323de9e8969a68f2e597d
xg4 <xingor4@gmail.com>
2023-07-18 07:31:20 +0000

    feat: simplify

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 /** @type {import("prettier").Options} */
 export default {
-  printWidth: 120,
   semi: false,
   singleQuote: true,
-  trailingComma: 'all',
-  arrowParens: 'avoid',
   plugins: ['prettier-plugin-tailwindcss'],
 }
